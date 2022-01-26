d2ab2e297d7954aa35e4f8ef596bf03c03c7e7f9
Ágoston Reischl <58388975+reischlago@users.noreply.github.com>
2022-01-26 21:56:22 +0100

    php próba

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "admin@devitems.com";
+        $recipient = "ragoston001@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
